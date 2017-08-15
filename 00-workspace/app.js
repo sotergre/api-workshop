f927d0d447c5e1e0a44ce380974888682d8aba21
sotergre <sotergre@users.noreply.github.com>
2017-08-15 16:46:27 -0400

    verbal post instructions

--- 00-workspace/app.js
+++ 00-workspace/app.js
@@ -108,7 +108,10 @@ var app = {
 
   clearList: function(e){
     app.options = [];
+    app.trip = {},
     app.renderResultsList();
+    app.renderDirectionsList();
+    app.routeLayer.setSource(null);
   },
 
   clearSearch: function(e){
